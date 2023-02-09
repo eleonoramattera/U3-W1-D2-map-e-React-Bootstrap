120c6d3afa856a9342ae574d2ce9a7ba39a4530e
Eleonora Mattera <m.eleonora98@live.it>
2023-02-09 12:35:29 +0100

    tutto

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import "./App.css";
+import AllTheBooks from "./components/AllTheBooks";
+import CustomNav from "./components/CustomNav";
+import Jumbotron from "./components/Jumbotron";
+import MyFooter from "./components/MyFooter";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <CustomNav />
+      <Jumbotron />
+      <AllTheBooks />
+      <MyFooter />
+    </>
   );
 }
 
